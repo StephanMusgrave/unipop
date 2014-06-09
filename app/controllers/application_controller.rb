3dfd81fd74f86012294f24f489cafc9c1de71db4
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-06-09 18:52:46 +0100

    Added auth for last_name as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) << :first_name
+  	devise_parameter_sanitizer.for(:sign_up) { | u | u.permit(:first_name, :last_name, :email, :password, :password_confirmation )}
   end
 
 end

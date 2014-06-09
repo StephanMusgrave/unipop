549880cff5eb6d566dcc54c9e7d7747d48c738e5
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-06-09 18:35:12 +0100

    Editing the user model and sign_up, added first_name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609161154) do
+ActiveRecord::Schema.define(version: 20140609170252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20140609161154) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

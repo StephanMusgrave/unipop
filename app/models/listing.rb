9223fcaa499be727f02d2c853b8e4f78af98ce14
Louise <louiselai88@gmail.com>
2014-06-10 18:59:20 +0100

    about to create joint table and other relationships, not yet started

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ActiveRecord::Base
 	has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
-  belongs_to_many :buyers, class_name: 'User', foreign_key: 'seller_id'
+
 end

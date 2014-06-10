a4e1297fd5ccc4edf2122bce1a37e5adc95f336d
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-06-10 18:08:45 +0100

    added user validation and required fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "spec/images/profile_picture.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+  validates_presence_of :first_name, :last_name
+
 end

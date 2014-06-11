b674c667a3b72fd37bafe969e62fabdfb030022a
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-06-11 18:06:14 +0100

    Refactored the cucumber test for adding product image

--- features/step_definitions/add_listing_steps.rb
+++ features/step_definitions/add_listing_steps.rb
@@ -6,7 +6,7 @@ end
 Then(/^I should be able to post a listing$/) do
 	expect(current_path).to eq '/listings/new'
   fill_in 'Description', with: 'Test'
-  attach_file('listing-picture',Rails.root.join('images/product_image.jpg'))
+  attach_file('Picture', Rails.root.join('public/images/product_image.jpg'))
   click_on 'Add my stuff'
   @listing = Listing.last.id
   expect(current_path).to eq "/listings/#{@listing}"
@@ -16,6 +16,7 @@ end
 Then(/^the listing should belong to seller$/) do
 	expect(Listing.last.seller.first_name).to eq "Ollie"
 end
+
 
 Then(/^I can see a picture$/) do
   expect(page).to have_css('img.listing-picture')

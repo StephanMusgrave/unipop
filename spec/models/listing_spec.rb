9fbefa5bb5d451c7b6b6ede1166d44e1b6666a67
Louise <louiselai88@gmail.com>
2014-06-13 10:25:54 +0100

    now all search inputs are case insensitive and also unique in the database

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -31,9 +31,21 @@ describe Listing do
 				expect(my_listing.hashtags.count).to eq 2
 			end
 
-			it 'adds two hashtags if separated by one space' do
+			it 'adds two hashtags if separated by just one space' do
 				my_listing.hashtag_names = 'bottle 750ml'
 				expect(my_listing.hashtags.count).to eq 2
+			end
+
+			it 'adds two hashtags if separated by a whole lot of edges cases' do
+				my_listing.hashtag_names = 'bottle 750ml   , new . filled  ..'
+				expect(my_listing.hashtags.count).to eq 4
+			end
+		end
+
+		context 'when the user inputs more than one non-unique hashtag' do
+			it 'only adds unique hastags to the database' do
+				my_listing.hashtag_names = 'bottle, bottle, Bottle, BOTTLE, BottLe'
+				expect(my_listing.hashtags.count).to eq 1
 			end
 		end
 

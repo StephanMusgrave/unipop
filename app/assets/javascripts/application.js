26df18c7e7b11cfec1852377a483ea571bad6eeb
Steve Musgrave <steve.musgrave@outlook.com>
2014-06-13 18:44:35 +0100

    js refactored

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 
 GMaps.geolocate({
   success: function(position) {
-    var user_location(position.coords.latitude, position.coords.longitude);
+    var user_location = [position.coords.latitude, position.coords.longitude];
   },
   error: function(error) {
     alert('Geolocation failed: '+error.message);

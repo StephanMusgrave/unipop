2abbc015e63072c6cddfa40cfe1014f524f38875
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-06-14 16:56:06 +0100

    Trying to print the distance using jQuery

--- app/assets/javascripts/distance_between.js
+++ app/assets/javascripts/distance_between.js
@@ -1,36 +1,50 @@
-// Haversine formula calculates the distance between two points (as the crow flies),
-// taking into account the curvature of the earth.
-// Returns a distance value in Kilometers.
-function haversine(lat1, lon1, lat2, lon2) {
-	var R = 6371; // km
-	var dLat = (lat2-lat1).toRad();
-	var dLon = (lon2-lon1).toRad();
-	var lat1 = lat1.toRad();
-	var lat2 = lat2.toRad();
+$(document).ready(function(){
+  var distance_from = $('#distance').data('listing-id');
+	GMaps.geocode({
+	  address: listing.location,
+	  callback: function(results, status) {
+	    if (status == 'OK') {
+	      var latlng = results[0].geometry.location;
+	      var @want_listing_lat = latlng.lat(),
+	      var @want_listing_lng = latlng.lng();
+	    }
+	  });
 
-	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-	        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-	var d = R * c; // Kilometers
-	var m = d * 0.621371192; // Miles
 
-	var rounded = roundVal(m);
+	// Haversine formula calculates the distance between two points (as the crow flies),
+	// taking into account the curvature of the earth.
+	// Returns a distance value in Kilometers.
+	function haversine(user_lat, user_lng, want_listing_lat, want_listing_lng) {
+	  var R = 6371; // km
+	  var dLat = (want_listing_lat-user_lat).toRad();
+	  var dLon = (want_listing_lng-user_lng).toRad();
+	  var lat1 = user_lat.toRad();
+	  var lat2 = want_listing_lat.toRad();
 
-	return rounded;
-}
+	  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+	          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(user_lat) * Math.cos(@want_listing_lat); 
+	  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	  var d = R * c; // Kilometers
+	  var m = d * 0.621371192; // Miles
 
-// Converts numeric degrees to radians
-if (typeof Number.prototype.toRad == 'undefined') {
-  Number.prototype.toRad = function() {
-    return this * Math.PI / 180;
-  }
-}
+	  var rounded = roundVal(m);
 
-// Rounds a number to 2 decimal places
-function roundVal(val) {
-	var dec = 2;
-	var result = Math.round(val*Math.pow(10,dec))/Math.pow(10,dec);
-	return result;
-}
+	  return rounded;
+	}
+
+	// Converts numeric degrees to radians
+	if (typeof Number.prototype.toRad == 'undefined') {
+	  Number.prototype.toRad = function() {
+	    return this * Math.PI / 180;
+	  }
+	}
+
+	// Rounds a number to 2 decimal places
+	function roundVal(val) {
+	  var dec = 2;
+	  var result = Math.round(val*Math.pow(10,dec))/Math.pow(10,dec);
+	  return result;
+	}
+});
 
 
---
+++ app/assets/javascripts/listings.js
@@ -0,0 +1,13 @@
+  GMaps.geolocate({
+  success: function(position) {
+    var @user_lat = position.coords.latitude,
+    var @user_lng = position.coords.longitude;
+  },
+  error: function(error) {
+    alert('Geolocation failed: '+error.message);
+  },
+  not_supported: function() {
+    alert("Your browser does not support geolocation");
+  }
+});
+

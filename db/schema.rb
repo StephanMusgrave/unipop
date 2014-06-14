b5a9a49632f9fba40c2d99e4d1cee2da8bfdb2b8
Steve Musgrave <steve.musgrave@outlook.com>
2014-06-14 14:26:06 +0100

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20140613150817) do
     t.datetime "avatar_updated_at"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "ip_address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

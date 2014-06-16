10bd5dc056ffdca367f8b7bdd0928970d91af740
Shelley Hope <shelleyjhope@gmail.com>
2014-06-16 12:20:59 +0100

    add and delete tests passing until picture testing steps

--- features/step_definitions/hashtag_steps.rb
+++ features/step_definitions/hashtag_steps.rb
@@ -8,8 +8,7 @@ When(/^I fill in the "(.*?)" field with "(.*?)"$/) do |field_name, stuff|
   fill_in(field_name, :with => stuff)
 end
 
-Then(/^I should be on the page of my listing$/) do
-	@listing = Listing.last.id
-  expect(current_path).to eq listing_path @listing
+Then(/^I should be on my "(.*?)"$/) do |arg1|
+  @listing = Listing.last.id
+  expect(current_path).to eq dashboard_show_path
 end
-

31998fa3362d6205ff57f5bde62ae854147c21f2
muhanad <muhanad40@gmail.com>
2014-06-16 20:07:08 +0100

    Profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,17 @@ class UsersController < ApplicationController
 	
 	def show
 		authenticate_user!
-		@user = User.find(current_user.id)
-		@user_listings = Listing.where(seller: @user)
+		# @user = User.find(current_user.id)
+		# @user_wants = Listing.where(buyer: @user)
+		# @user_selling = Listing.where(seller: @user)
+		if params[:id]
+			user_id = params[:id]
+		else
+			user_id = current_user.id
+		end
+		@user_info = User.find(user_id)
+		@user_wants = @user_info.want_listings
+		@user_selling = @user_info.sales_listings
 	end
 
 end

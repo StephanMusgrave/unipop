f503ff94f4f9c958a5511a4782a4b34fa40820f3
Louise <louiselai88@gmail.com>
2014-06-18 15:52:41 +0100

    jQuery added to chatroom page, and formatiing

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,9 +1,10 @@
-// $(document).ready(function(){
-// 	 $(document).keypress(function(e){
-// 	 	if (e.which == 13) {
-// 	 	e.preventDefault();
-// 	 	var chatMessage = $('input[name=comment]').val(); 
-// 	 	$('ul').append("<li>" + chatMessage + "</li>");
-// 	 		}
-//     });
-// 	});
+$(document).ready(function(){
+	 $(document).keypress(function(e){
+	 	if (e.which == 13) {
+	 	e.preventDefault();
+	 	var commentBody = $('textarea[id=comment_body]').val();
+	 	var
+	 		 	$('.chatroom-comments-container').append("<p>" + commentBody + "</p>");
+	 		}
+    });
+	});

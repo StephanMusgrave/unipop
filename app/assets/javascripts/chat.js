510554c87ee5960baa5389c79fc539076472d40e
muhanad <muhanad40@gmail.com>
2014-06-19 10:05:57 +0100

    Improvements to chat js

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
 	var listing_id = $('.new_comment').data('listing-id');
 	$(".new_comment").on('submit', function(event) {
 		event.preventDefault();
+		var message = $('#comment_body').val();
+		if(message.length < 1) return;
 		$.ajax({
 			url: '/listings/' + listing_id + '/chatroom/comments',
 			data: $(this).serialize(),
 			method: 'POST'
 		}).fail(function() {
 			alert('Error: Chat message was not delivered!');
+		}).always(function() {
+			$('#comment_body').val('');
 		});
 	});
+
 	var connection = new WebSocketRails(window.location.host + '/websocket');
   channel = connection.subscribe('chat_comments');
   channel.bind('new', function(comment) {
